8ad2445b11ea57f9125379b607a4f29dc2815671
pittayutkhorniewklang <pittayut.k@ku.th>
2024-11-03 11:22:50 +0700

    fix

--- controllers/routesController.js
+++ controllers/routesController.js
@@ -6,9 +6,12 @@ exports.getRoutes = async (req, res) => {
     const pool = await poolPromise;
     const result = await pool.request().query(`
       SELECT 
+        Trips.id AS trip_id,
         Routes.start,
         Routes.end_point,
         Trips.departure_time,
+        Trips.arrival_time,
+        Trips.available_seats,
         Routes.ticket_price
       FROM Trips
       INNER JOIN Routes ON Trips.route_id = Routes.id;
@@ -21,20 +24,25 @@ exports.getRoutes = async (req, res) => {
 
 // เพิ่มเส้นทางใหม่ลงในฐานข้อมูล
 exports.addRoute = async (req, res) => {
-  const { start, end_point, ticket_price, departure_time, arrival_time, available_seat } = req.body;
+  const { start, end_point, ticket_price, departure_time, arrival_time, available_seats } = req.body;
   try {
-    const pool = await poolPromise;
-    await pool.request()
-      .input('start', sql.NVarChar, start)
-      .input('end_point', sql.NVarChar, end_point)
-      .input('ticket_price', sql.Float, ticket_price)
-      .input('departure_time', sql.DateTime, departure_time)
-      .input('arrival_time', sql.DateTime, arrival_time)
-      .input('available_seat', sql.Int, available_seat)
-      .query('INSERT INTO Routes (start, end_point, ticket_price, departure_time, arrival_time, available_seat) VALUES (@start, @end_point, @ticket_price, @departure_time, @arrival_time, @available_seat)');
-    res.status(201).json({ message: 'Route added successfully' });
+      const pool = await poolPromise;
+      await pool.request()
+          .input('start', sql.NVarChar, start)
+          .input('end_point', sql.NVarChar, end_point)
+          .input('ticket_price', sql.Float, ticket_price)
+          .input('departure_time', sql.DateTime, departure_time)
+          .input('arrival_time', sql.DateTime, arrival_time)
+          .input('available_seats', sql.Int, available_seats)
+          .query(`
+              INSERT INTO Trips (route_id, departure_time, arrival_time, available_seats)
+              VALUES ((SELECT id FROM Routes WHERE start = @start AND end_point = @end_point AND ticket_price = @ticket_price), 
+                      @departure_time, @arrival_time, @available_seats)
+          `);
+      res.status(201).json({ message: 'Route added successfully' });
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    console.error('Error:', err);
+      res.status(500).json({ message: err.message });
   }
 };
 
